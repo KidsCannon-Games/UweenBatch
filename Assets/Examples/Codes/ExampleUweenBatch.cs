a3e551295537702f2c6dace0d7303392fcb1f87a
Takatoshi Matsumoto <toqoz403@gmail.com>
2016-10-06 02:06:15 +0900

    :rocket: rough sketch

---
+++ Assets/Examples/Codes/ExampleUweenBatch.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+using Uween;
+
+public class ExampleUweenBatch : MonoBehaviour {
+	public List<GameObject> spriteRenderers;	
+
+	void Start () {
+		var batch = new UweenBatch();
+
+		spriteRenderers.ForEach(go => {
+			batch.Register(TweenY.Add(go, Random.Range(0.5f, 1.5f), Camera.main.orthographicSize).Delay(Random.Range(0.5f, 1.5f)));
+		});
+
+		batch.Execute().Then(() => {
+			spriteRenderers.ForEach(go => {
+				TweenY.Add(go, 0.5f, 0).Delay(0.2f);
+			});
+		});
+	}
+}
---
+++ Assets/UweenBatch/UweenBatch.cs
@@ -0,0 +1,40 @@
+﻿using Uween;
+using System.Collections.Generic;
+
+public class UweenBatch {
+	private List<Tween> tweens = new List<Tween>();
+	private bool executing = false;
+	public event Uween.Callback OnComplete;
+
+	public UweenBatch Register(Tween tw) {
+			tweens.Add(tw);
+			tw.enabled = false;
+			return this;
+	}
+
+	public UweenBatch Execute() {
+		if (executing) {
+			OnComplete();
+		} else {
+			executing = true;
+			var finishCount = 0;
+
+			tweens.ForEach(tw => {
+				tw.enabled = true;
+				tw.Then(() => {
+					finishCount++;
+					if (finishCount == tweens.Count) {
+						executing = false;
+						OnComplete();
+					}
+				});
+			});
+		}
+		return this;
+	}
+
+	public UweenBatch Then(Uween.Callback callback) {
+		OnComplete += callback;
+		return this;
+	}
+}
